8ddb255eaf09a142699bf7b70535dcd0c733e197
vanson159 <nguyenthaisonpo155@gmail.com>
2023-03-20 07:20:30 +0000

    package-variable-function

--- package-variable-function/variables/main.go
+++ package-variable-function/variables/main.go
@@ -2,6 +2,18 @@ package main
 
 import "fmt"
 
+// explicit variable
+var c, python, java bool
+
+// implicit variable
+var name = "Nguyen "
+
+// variable with initializers
+var i, j, l int = 1, 2, 3
+var age, handsome, mode = 2, true, "test"
+
 func main() {
-	fmt.Print("Hello world!")
+	// short variable declaration - implicit variable v2 (only used in function level)
+	lastname := "Son"
+	fmt.Print(lastname)
 }
